8cf9f232851b3a62b45e4414560dd6d7168a6bd2
ezys <viv@mail.ru>
2018-05-05 23:49:53 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require ('express');
+    bodyParser = require('body-parser');
+    app = express();
+
+app.use(bodyParser.urlencoded({ extended:true}));
+app.use( bodyParser.json() );
+app.get("/", function (req, res) {
+    res.render("index.ejs");
+} );
+
+app.listen(3000, function() {
+    console.log('Work on port: 3000');
+});
